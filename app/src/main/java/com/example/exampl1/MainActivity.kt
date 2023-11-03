c1f09fb88fad18a7e7f0ea8fda385a35266a8b3c
ppm <usumanpm@gmail.com>
2023-11-03 18:41:14 +0900

    sfsdf

--- app/src/main/java/com/example/exampl1/MainActivity.kt
+++ app/src/main/java/com/example/exampl1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.exampl1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bt: Button =findViewById(R.id.button)
+        val txt: TextView =findViewById(R.id.txt)
+        val edt: EditText =findViewById(R.id.editTextText)
+        bt.setOnClickListener(){
+            txt.text=edt.text
+        }
+
     }
 }
